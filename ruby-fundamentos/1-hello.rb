8a68280dd1ef632bc91025aaf7e9d0a20e2a01db
Rodrigo Borge <rodrigo@borge.com.br>
2024-03-19 22:57:04 -0300

    Variáve e tipos  de dados

--- ruby-fundamentos/1-hello.rb
+++ ruby-fundamentos/1-hello.rb
@@ -3,6 +3,8 @@ print 'hello world'
 print 'olá mundo'
 puts 'hello world'
 puts 'olá mundo'
+
 =begin
 Estou aprendendo a linguagem Ruby na One Bit Code
 end
+
---
+++ ruby-fundamentos/2-tipos.rb
@@ -0,0 +1,13 @@
+# Dados do Filme
+name = 'Avatar'
+yearLaunch = 2022
+price = 100.00
+planIncluded = false
+
+puts name, yearLaunch, price, planIncluded
+
+# Identificando Tipos de Dados
+puts name.class
+puts yearLaunch.class
+puts price.class
+puts planIncluded.class
